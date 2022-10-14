59bbdf4c11433fa6a9d5cd5c8caf5967bac2fe38
Rabah GHILES <rabahghiles@gmail.com>
2022-10-14 17:33:09 +0200

    Fix bug du rooting vers la vue offers

--- front/src/App.js
+++ front/src/App.js
@@ -11,8 +11,8 @@ import OffersService from './services/offers.service';
 function App() {
   return <Routes>
     <Route path='/' element={<MainTemplate />} >
-      <Route element={<OffersListView OffersService={new OffersService()} />} />
-      <Route path="offers" element={<OffersListView OffersService={new OffersService()} />} />
+      <Route path="/" element={<OffersListView OffersService={new OffersService()} />} />
+      <Route path="/offers" element={<OffersListView OffersService={new OffersService()} />} />
       <Route path='/offers/:id' element={<OfferDetailsView OffersService={new OffersService} />} ></Route>
     </Route>
   </Routes>
