1a08cc7c911a18916e80d92a0dd49ba5e5fadac6
Rabah GHILES <rabahghiles@gmail.com>
2022-10-21 11:59:26 +0200

    Config de deploy

--- front/src/services/offers.service.js
+++ front/src/services/offers.service.js
@@ -3,14 +3,14 @@ import axios from "axios";
 
 class OffersService {
 
-    #API_URL = "https://localhost:7201/api";
+    #API_URL = "https://app-soat-bc22-dev-fr.azurewebsites.net/api";
 
     getOffers() {
 
         return new Promise((resolve, reject) => {
             axios.get(`${this.#API_URL}/offers`)
             .then((response) => {
-                resolve(response.data.offers)
+                resolve(response.data.items)
             })
             .catch((error) => reject(error));
         })
